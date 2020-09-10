7193e5e6e1295ce72ef8200012c861be5b896551
Alex Chi <iskyzh@gmail.com>
2020-09-10 12:44:56 +0800

    update integration test

--- integration_test.sh
+++ integration_test.sh
@@ -4,6 +4,4 @@ set -ex
 
 curl -fsSL http://localhost/ > /dev/null
 curl -fsSL http://localhost/course-plus > /dev/null
-curl -fsSL http://localhost/subjects > /dev/null
-curl -fsSL http://localhost/websites > /dev/null
 curl -fsSL http://localhost/admin > /dev/null
