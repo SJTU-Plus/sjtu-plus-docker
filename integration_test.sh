c351b2af9ce59626002e163ba7c694e593a6a0f2
Alex Chi <iskyzh@gmail.com>
2020-09-10 12:05:46 +0800

    fix ci

---
+++ integration_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+curl -sSL http://localhost:8080/
+curl -sSL http://localhost:8080/course-plus
+curl -sSL http://localhost:8080/subjects
+curl -sSL http://localhost:8080/websites
+curl -sSL http://localhost:8080/admin
