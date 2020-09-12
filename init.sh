c2e0abd774004588a89b6c8abdcfe7d0a4f81a42
Alex Chi <iskyzh@gmail.com>
2020-09-12 17:29:37 +0800

    add sjtu-exporter (#6)
    * add sjtu-exporter
    
    * update file
    
    * update docker compose

--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ set -e
 git clone https://github.com/SJTU-Plus/course-plus statics/course-plus
 git clone https://github.com/SJTU-Plus/course-plus-data statics/course-plus-data
 git clone https://github.com/SJTU-Plus/sjtu-plus sjtu-plus
+git clone https://github.com/SJTU-Plus/sjtu_traffic_exporter services/sjtu_traffic_exporter
--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ echo "updating course-plus..."
 (cd statics/course-plus && git fetch && git reset --hard origin/gh-pages)
 echo "updating course-plus-data..."
 (cd statics/course-plus-data && git fetch && git reset --hard origin/master)
+echo "updating sjtu_traffic_exporter..."
+(cd services/sjtu_traffic_exporter && git fetch  && git reset --hard origin/master)
 echo "updating sjtu-plus..."
 (cd sjtu-plus && git pull)
 echo "rebuild sjtu-plus..."
