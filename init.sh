c8f52b1649995f3675d794540ec1618918502d8f
Alex Chi <iskyzh@gmail.com>
2020-09-13 12:53:29 +0800

    add libsjtu (#9)

--- init.sh
+++ init.sh
@@ -6,3 +6,4 @@ git clone https://github.com/SJTU-Plus/course-plus statics/course-plus
 git clone https://github.com/SJTU-Plus/course-plus-data statics/course-plus-data
 git clone https://github.com/SJTU-Plus/sjtu-plus sjtu-plus
 git clone https://github.com/SJTU-Plus/sjtu_traffic_exporter services/sjtu_traffic_exporter
+git clone https://github.com/SJTU-Plus/libsjtu/ statics/libsjtu
