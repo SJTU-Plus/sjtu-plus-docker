60c6202a219b7ba8aaa98eeb27b0a382f12a14a3
danyang685 <496298685@qq.com>
2020-09-13 15:50:24 +0800

    improve init.sh, add deinit.sh

--- init.sh
+++ init.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-git clone https://github.com/SJTU-Plus/course-plus statics/course-plus
-git clone https://github.com/SJTU-Plus/course-plus-data statics/course-plus-data
-git clone https://github.com/SJTU-Plus/sjtu-plus sjtu-plus
-git clone https://github.com/SJTU-Plus/sjtu_traffic_exporter services/sjtu_traffic_exporter
-git clone https://github.com/SJTU-Plus/libsjtu/ statics/libsjtu
+[ ! -d "statics/course-plus" ] && git clone --depth=1 --branch gh-pages https://github.com/SJTU-Plus/course-plus statics/course-plus
+[ ! -d "statics/course-plus-data" ] && git clone --depth=1 https://github.com/SJTU-Plus/course-plus-data statics/course-plus-data
+[ ! -d "sjtu-plus" ] && git clone --depth=1 https://github.com/SJTU-Plus/sjtu-plus sjtu-plus
+[ ! -d "services/sjtu_traffic_exporter" ] && git clone --depth=1 https://github.com/SJTU-Plus/sjtu_traffic_exporter services/sjtu_traffic_exporter
+[ ! -d "statics/libsjtu" ] && git clone --depth=1 --branch gh-pages https://github.com/SJTU-Plus/libsjtu/ statics/libsjtu
+
+echo "Finished!"
