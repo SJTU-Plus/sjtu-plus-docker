ba91e9776c6da80bb5c5106bec1a6ecfadbbe218
danyang685 <496298685@qq.com>
2020-10-17 11:46:43 +0000

    fix: specify branch manually to prevent git warning

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@
 set -e
 
 [ ! -d "statics/course-plus" ] && git clone --depth=1 --branch gh-pages https://github.com/SJTU-Plus/course-plus statics/course-plus
-[ ! -d "statics/course-plus-data" ] && git clone --depth=1 https://github.com/SJTU-Plus/course-plus-data statics/course-plus-data
-[ ! -d "sjtu-plus" ] && git clone --depth=1 https://github.com/SJTU-Plus/sjtu-plus sjtu-plus
-[ ! -d "services/sjtu_traffic_exporter" ] && git clone --depth=1 https://github.com/SJTU-Plus/sjtu_traffic_exporter services/sjtu_traffic_exporter
+[ ! -d "statics/course-plus-data" ] && git clone --depth=1 --branch master https://github.com/SJTU-Plus/course-plus-data statics/course-plus-data
+[ ! -d "sjtu-plus" ] && git clone --depth=1 --branch master https://github.com/SJTU-Plus/sjtu-plus sjtu-plus
+[ ! -d "services/sjtu_traffic_exporter" ] && git clone --depth=1 --branch master https://github.com/SJTU-Plus/sjtu_traffic_exporter services/sjtu_traffic_exporter
 [ ! -d "statics/libsjtu" ] && git clone --depth=1 --branch gh-pages https://github.com/SJTU-Plus/libsjtu/ statics/libsjtu
 
 echo "Finished!"
