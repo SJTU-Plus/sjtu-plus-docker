a0d93f734b29841e05a720275fb070e184e34181
Alex Chi <iskyzh@gmail.com>
2021-04-26 19:15:09 +0800

    add sjtu-plus tools (#29)

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 set -e
 
 [ ! -d "statics/course-plus" ] && git clone --depth=1 --branch gh-pages https://github.com/SJTU-Plus/course-plus statics/course-plus
+[ ! -d "statics/tools" ] && git clone --depth=1 --branch gh-pages https://github.com/SJTU-Plus/tools statics/tools
 [ ! -d "statics/course-plus-data" ] && git clone --depth=1 --branch master https://github.com/SJTU-Plus/course-plus-data statics/course-plus-data
 [ ! -d "sjtu-plus" ] && git clone --depth=1 --branch master https://github.com/SJTU-Plus/sjtu-plus sjtu-plus
 [ ! -d "services/sjtu_traffic_exporter" ] && git clone --depth=1 --branch master https://github.com/SJTU-Plus/sjtu_traffic_exporter services/sjtu_traffic_exporter
